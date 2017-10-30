1f803bf3b516ab8bd8d1ea870b12fb7a305b6207
diego-carvalho <diego.a.carvalho@hotmail.com>
2017-10-30 14:33:12 -0200

    end version

--- browser.py
+++ browser.py
@@ -8,8 +8,15 @@ if len(sys.argv) < 1:
     print 'comando de execução faltando parametro, exemplo: python browser.py index.html 8080'
     sys.exit(1)
 else:
-    path = sys.argv[1].split('/')
+
+    path = sys.argv[1]
+    if 'http' in path:
+        path = path.split('://')[1]
+
+    path = path.split('/')
+
     bind_ip = path[0]
+
     file_r = "/".join(path[1:])
 
     if len(sys.argv) == 3:
@@ -17,10 +24,16 @@ else:
     else:
         bind_port = 80
 
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((bind_ip, bind_port))
 
-client.send('GET /' +file_r+ ' HTTP/1.1\r\nHost: {}.{}\r\n\r\n'.format('dcomp', 'ufsj.edu.br'))
+
+s = "GET /" + file_r + " HTTP/1.1\nHost: " + bind_ip + ":" + str(bind_port) + "\nConnection: keep-alive\nCache-Control: max-age=0\nUser-Agent: Mozilla/5.0(X11;Linux x86_64) AppleWebKit/537.36(KHTML, like Gecko) Chrome/62.0.3202.75Safari/537.36\nUpgrade-Insecure-Requests: 1\nAccept: text/html, application/xhtml + xml, application/xml;q=0.9, image/webp, image/apng, /;q=0.8\nAccept-Language: pt-BR, pt;q=0.9, en-US;q=0.8, en;q=0.7\n\n\n"
+
+
+#client.send('GET /' +file_r+ ' HTTP/1.1\r\nHost: {}.{}\r\n\r\n'.format('dcomp', 'ufsj.edu.br'))
+client.send(s)
 
 response = client.recv(1024)
 
@@ -36,5 +49,5 @@ else:
         f = file_r.split('/')
         file_out = f[len(f) - 1]
         file_o = open('downloads/'+file_out, 'w')
-        file_o.write("".join(response.split('\n\n')[1]))
+        file_o.write("".join(response.split('\n\n')[1:]))
     print response
